4a1df380c8ed2d6059fec989b326e0e251579f25
josefusfus <jlsaez@jlsaez.es>
2020-02-08 11:57:46 +0100

    Ionic project hello World with two pictures

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  src = 'https://miro.medium.com/max/1200/1*3ILWYysD5A3wSiEUCAS4oQ.jpeg';
+  description = 'Obi Wan Kenobi';
+
+  funcionPrueba(): number {
+    let numero: number;
+    numero = 3;
+    return numero;
+  }
 
   constructor() {}
 
